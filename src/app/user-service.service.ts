3fbb07a576e4d817ac916215f07b25a311d8185d
robajillo <rjillo@yahoo.com>
2020-08-29 21:34:20 +0300

    imported user repository, http client, and environment from their respective files

--- src/app/user-service.service.ts
+++ src/app/user-service.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
-
+import { User } from './user';
+import { Repository } from './repository';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserServiceService {
-
-  constructor() { }
+  constructor() {}
 }
